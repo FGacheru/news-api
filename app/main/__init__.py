e8b3656c81e316f656094096ed9ce9e75783e627
b-ndunda <francisgacheru2001@gmail.com>
2020-11-29 11:09:00 +0300

    create a function create_app

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -2,9 +2,9 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
 
-# bootstrap = Bootstrap()
+bootstrap = Bootstrap()
 
-# def create_app(config_name):
+def create_app(config_name):
 
 #     app = Flask(__name__)
 
