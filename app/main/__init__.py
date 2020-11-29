c72cdeece21137730cca7a5f78ede88451cafce8
b-ndunda <francisgacheru2001@gmail.com>
2020-11-29 11:09:56 +0300

    form an instance of app module

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -6,14 +6,14 @@ bootstrap = Bootstrap()
 
 def create_app(config_name):
 
-#     app = Flask(__name__)
+    app = Flask(__name__)
 
-#     # Creating the app configurations
-#     app.config.from_object(config_options[config_name])
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
 
-#     # Initializing flask extensions
-#     bootstrap.init_app(app)
+    # Initializing flask extensions
+    bootstrap.init_app(app)
 
-#     # Will add the views and forms
+    # Will add the views and forms
 
-#     return app
+    return app
