029a94ae4a4bf31aebe7d508b95201f004aff685
b-ndunda <francisgacheru2001@gmail.com>
2020-11-29 13:04:36 +0300

    add one argument on the news model and created articles model

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ class news:
     News class to define news object
     '''
     
-    def __init__(self,title,author,image,description,date,articles):
+    def __init__(self,title,id,author,image,description,date,articles):
         self.title = title
         self.author = author
         self.image = image
@@ -12,4 +12,10 @@ class news:
         self.articles = articles
         
         
-        
+class articles:
+    
+    all_articles = []  
+    
+    def __init__(self,title,id,author,image,description,date,articles)
+    
+            
