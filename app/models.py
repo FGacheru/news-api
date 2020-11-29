d053923341f8a17b7c1dc5d8cfa2b228998a97c3
b-ndunda <francisgacheru2001@gmail.com>
2020-11-29 12:55:49 +0300

    pass 6 arguments on function init in the news class

---
+++ app/models.py
@@ -0,0 +1,15 @@
+class news:
+    '''
+    News class to define news object
+    '''
+    
+    def __init__(self,title,author,image,description,date,articles):
+        self.title = title
+        self.author = author
+        self.image = image
+        self.description = description
+        self.date = date
+        self.articles = articles
+        
+        
+        
--- config.py
+++ config.py
@@ -8,12 +8,23 @@ class Config:
 
 
 class ProdConfig(Config):
+    """
+    production configuration child clas
+
+    Args:
+       config: The parent configuration class with general configuration settings
+    """
     pass
 
-
 class DevConfig(Config):
-    DEBUG = True
+    """
+    development configuration child class
 
+    Args:
+        Config: The parent configuration class with General configuration settings.
+    """
+    DEBUG = True
+    
 config_options = {
 'development':DevConfig,
 'production':ProdConfig
