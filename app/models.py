4f4dc962e5b8679475a31d0d01e5b7ca65f4d55a
b-ndunda <francisgacheru2001@gmail.com>
2020-12-01 00:39:40 +0300

    add two sources

--- app/models.py
+++ app/models.py
@@ -1,21 +1,23 @@
-class news:
+class Sources:
     '''
-    News class to define news object
+    Sources class to define Sources Objects
     '''
-    
-    def __init__(self,title,id,author,image,description,date,articles):
-        self.title = title
-        self.author = author
-        self.image = image
+    def __init__(self,id,name,description,url,category):
+        self.id =id
+        self.name = name
         self.description = description
+        self.url = url
+        self.category = category
+class Articles:
+    '''
+    Articles class to define articles objects
+    '''
+    def __init__(self,id,author,title,description,url,image,date):
+        self.id = id
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.image = image
         self.date = date
-        self.articles = articles
-        
-        
-class articles:
-    
-    all_articles = []  
-    
-    def __init__(self,title,id,author,image,description,date,articles)
-    
-            
+
