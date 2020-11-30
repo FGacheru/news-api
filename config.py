a17a7984a5c2880878f37591f5c83570f19062c9
b-ndunda <francisgacheru2001@gmail.com>
2020-12-01 00:36:08 +0300

    created two classes

--- config.py
+++ config.py
@@ -1,31 +1,20 @@
 import os
-
 class Config:
-
-    NEWS_API_BASE_URL ='http://newsapi.org/v2/everything?q={}&apiKey={}'
+    '''
+    General configuration parent class
+    '''
+    NEWS_SOURCES_BASE_URL ='https://newsapi.org/v2/sources?apiKey=5810c4b503854ab0bb9be6639d73a30a'
+    ARTICLES_BASE_URL = 'https://newsapi.org/v2/everything?q=bitcoin&apiKey=5810c4b503854ab0bb9be6639d73a30a'
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
-
-
+    @staticmethod
+    def init_app(app):
+            pass
 class ProdConfig(Config):
-    """
-    production configuration child clas
-
-    Args:
-       config: The parent configuration class with general configuration settings
-    """
     pass
-
 class DevConfig(Config):
-    """
-    development configuration child class
-
-    Args:
-        Config: The parent configuration class with General configuration settings.
-    """
     DEBUG = True
-    
 config_options = {
-'development':DevConfig,
-'production':ProdConfig
+    'development': DevConfig,
+    'production': ProdConfig
 }
